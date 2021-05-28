1c8e8f198061d1f9b277e8f7036a8e26ec3d96da
Zouhair Loucif <zouhair.loucif@it.devcorp.fr>
2021-05-28 18:41:11 +0100

    added-ui-elements
    Added UI elements to to match the requested demands
    added logic to send request on first loading of the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,88 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <template v-if="isLoading">
+            <div class="text-center">
+              <v-progress-circular class="mx-auto" :size="50" :width="3" indeterminate/>
+            </div>
+          </template>
+          <template v-else>
+            <v-list three-line>
+              <template v-for="(item, index) in getStarredReposList">
+                <div :key="index">
+                  <v-list-item :href="item.url" target="_blank">
+                    <v-list-item-avatar size="110px">
+                      <v-img :src="item.owner.avatar"></v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title v-html="item.name"></v-list-item-title>
+                      <v-list-item-subtitle v-html="item.description"></v-list-item-subtitle>
+                      <v-list-item-group>
+                        <v-item-group>
+                          <v-item>
+                            <span>Stars: {{item.stars | numberFormatter}}</span>
+                          </v-item>
+                          <v-item>
+                            <span>Issues: {{item.issues | numberFormatter}}</span>
+                          </v-item>
+                          <v-item>
+                            <span>Submitted {{$moment(item.created_at).fromNow() }} by {{item.owner.name}}</span>
+                          </v-item>
+                        </v-item-group>
+                      </v-list-item-group>
+                    </v-list-item-content>
+                  </v-list-item>
+                </div>
+              </template>
+            </v-list>
+          </template>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import {mapActions, mapGetters} from "vuex";
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      isLoading: false,
+      form: {
+        created: {
+          operator: '<',
+          value: this.$moment().format('YYYY-MM-DD')
+        },
+        sort: "stars",
+        order: "desc",
+        page: 1,
+      }
+    }
+  },
+  computed: {
+    ...mapGetters('reposService', [
+      'getStarredReposList',
+    ])
+  },
+  methods: {
+    ...mapActions('reposService', {
+      getStarredReposAction: 'getStarredRepos',
+    }),
+    getReposFromApi() {
+      this.isLoading = true;
+      let params = {...this.form};
+      this.getStarredReposAction(params)
+        .finally(() => {
+          this.isLoading = false;
+        })
+    }
+  },
+  created() {
+    this.getReposFromApi();
   }
 }
 </script>
