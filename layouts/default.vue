1d4591aee0e91b3bf453871f80c1cdae94ee9523
Zouhair Loucif <zouhair.loucif@gmail.com>
2021-05-29 18:47:20 +0100

    Hide the logo on small screens

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app dark>
     <v-app-bar fixed app elevation="0">
-      <v-toolbar-title v-text="title" />
+      <v-avatar size="50px" class="mr-4" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-img src="/logo.png"></v-img>
+      </v-avatar>
+      <v-toolbar-title v-text="title"/>
       <v-spacer></v-spacer>
       <v-icon @click="changeTheme">mdi-moon-waxing-crescent</v-icon>
     </v-app-bar>
